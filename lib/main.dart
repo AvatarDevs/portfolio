0dde9db09e42117f7591d056392667804731e5e2
Colby <colbymaloydev@gmail.com>
2019-10-18 05:26:38 -0500

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:portfolio/layout/desktop.dart';
+import 'package:portfolio/layout/mobile.dart';
+
+//This is an app intended to be a portfolio site responsive to Screen sizes by following principles found here
+// -https://medium.com/flutter-community/developing-for-multiple-screen-sizes-and-orientations-in-flutter-fragments-in-flutter-a4c51b849434
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: (BuildContext context, BoxConstraints constraints) {
+        return constraints.maxWidth > 650
+            ? _buildDesktopUI()
+            : _buildMobileUI();
+      },
+    );
+  }
+
+  Widget _buildMobileUI() {
+    return MobileLayout();
+  }
+
+  Widget _buildDesktopUI() {
+    return DesktopLayout();
+  }
+}
